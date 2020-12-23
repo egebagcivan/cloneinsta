3884eccc926abce65b707c1294d9b273604dbb36
Ege <ege.bagcivan@mudimedia.comm>
2020-12-23 15:21:26 +0300

    deneme

---
+++ src/pages/PageCamera.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-page class="constrain q-pa-md">
+    <div class="camera-frame q-pa-md">
+      <img class="full-width" src="https://cdn.quasar.dev/img/parallax2.jpg"/>
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageCamera'
+}
+</script>
+<style lang="sass">
+  .camera-frame
+    border: 2px solid
+</style>
